7ec84924a5954be5809c1efbe523762b60f50e5e
Bridgette <turner.bridgette@yahoo.com>
2020-12-29 21:26:09 -0700

    buttons

--- src/components/countries/country/Country.js
+++ src/components/countries/country/Country.js
@@ -2,10 +2,32 @@ import React from "react";
 
 export default function Country(props) {
   console.log("Country:", props.country);
+  let languages;
+  languages =
+    props.country !== ""
+      ? Object.entries(props.country.languages).map((k, v) => {
+          return k;
+        })
+      : null;
+  console.log(languages);
   return (
     <div>
-      <h1>Hello World</h1>
-      {props.country !== "" ? <h2>{props.country.name}</h2> : null}
+      {props.country !== "" ? (
+        <>
+          <h2>{props.country.name}</h2>
+          <img src={props.country.flag.file} alt="flag" width="200px" />
+          <p>Capital: {props.country.capital.name}</p>
+          <p>Currency: {props.country.currency.name}</p>
+          <ul>
+            Languages:
+            {languages.map((l) => (
+              <li>{l[1]}</li>
+            ))}
+          </ul>
+          <p>Time: {props.country.timezone.time}</p>
+          <p>Timezone: {props.country.timezone.timezone}</p>
+        </>
+      ) : null}
     </div>
   );
 }
